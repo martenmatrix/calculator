a153847a5d08a4ae44af88069a04cd7e0630e8cd
Marten <derapfelesser@aol.com>
2021-08-07 01:22:05 +0200

    The code worked in the debugger on the beat drop [Can calcalute the digits in the array now]

--- js/script.js
+++ js/script.js
@@ -16,8 +16,6 @@ function divide(num1, num2) {
 
 function operate(operator,num1, num2) {
 
-    if (operatorsUsed === 0) return num2;
-
     let result;
 
     if (operator == '+') {
@@ -59,6 +57,48 @@ function addToHistoryArray(charac) {
     typeHistory.push(charac);
 };
 
+function isOperator(string) {
+    if (string == '+' || string == '-' || string == '/' || string == '*') {
+        return true; 
+    } else  {
+        return false;
+    };
+}
+
+function calculateArrayHistory() {
+
+    let currentScore = (typeHistory.splice(0, 1))[0];
+    //start value of total is the first values as it is not processed
+
+    let currentOperator;
+    let currentNumber;
+
+    let total = typeHistory.reduce((total, currentEntry) => {
+        console.log(1);
+        if (isOperator(currentEntry)) {
+            currentOperator = currentEntry;
+        } else {
+            currentNumber = currentEntry;
+        };
+
+        if (!(currentOperator == undefined) && !(currentNumber == undefined)) {
+
+            let result = operate(currentOperator, total, currentNumber);
+
+            //reset values
+            currentScore = undefined;
+            currentNumber = undefined;
+
+            return result;
+        };
+
+        return total;
+
+    }, currentScore);
+
+    return total;
+};
+
 function getLastHistory() { 
     lengthOfArray = typeHistory.length;
     if (lengthOfArray == 0) return undefined;
@@ -86,17 +126,16 @@ function addEventListeners() {
         addToHistoryArray(parseFloat(currentNumber));
         addToHistoryArray(e.target.id);
         };
-        
+
         clearDisplay();
         currentNumber = "";
-
-
     }))
 };
 
 let typeHistory = [];
+
+//displayed on display
 let currentNumber;
-let currentScore;
 addEventListeners();
 
 //event listener to operators
